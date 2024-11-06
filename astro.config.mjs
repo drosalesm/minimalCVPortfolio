2dc414073f316203980ef29b962406b4fe0c5cac
David Rosales Mejia <drosalesm204@gmail.com>
2024-11-06 15:24:45 -0600

    fix tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   // Make sure your configuration is correct
   outDir: 'dist',
+  integrations: [tailwind()]
 });
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: [
+	  './src/**/*.{astro,js,jsx,ts,tsx}', 
+	  './components/**/*.{astro,js,jsx,ts,tsx}', 
+	  './pages/**/*.{astro,js,jsx,ts,tsx}'
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  };
+  
